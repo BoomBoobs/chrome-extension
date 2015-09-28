f02305c325da7b9983c8e3e657c9071aa1b1aa5a
Andrea Ascari <andrea.ascari@buildo.io>
2015-09-28 22:55:18 +0200

    Add update.xml and declared in manifest.json.

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const join = require('path').join;
 
 const crx = new ChromeExtension({
+  codebase: 'https://github.com/BoomBoobs/chrome-extension/raw/master/package/boomboobs.crx',
   privateKey: fs.readFileSync(join(__dirname, 'package', 'key.pem'))
 });
 
@@ -13,9 +14,8 @@ crx
     return crx.pack()
     .then((crxBuffer) => {
       try {
-        // var updateXML = crx.generateUpdateXML();
-        // console.log(join(__dirname, 'package', 'update.xml'));
-        // fs.writeFile(join(__dirname, 'package', 'updateXML', updateXML));
+        const updateXML = crx.generateUpdateXML();
+        fs.writeFile(join(__dirname, 'package', 'update.xml'), updateXML);
         fs.writeFile(join(__dirname, 'package', 'boomboobs.crx'), crxBuffer);
       } catch (e) {
         console.log(e);
